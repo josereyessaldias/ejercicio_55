afd984f08750ac04d80e3efef03882e17882fd68
josereyessaldias <jpreyess@uc.cl>
2019-05-18 17:52:36 -0400

    error arreglado

--- app/controllers/User/omniauth_callbacks_controller.rb
+++ app/controllers/User/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@
 class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    @user = User.from_omniauth(request.env("omniauth.auth"))
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user
