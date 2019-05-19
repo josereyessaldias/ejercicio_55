19a85e59cbacafa03b3428370855d40100c13b9f
josereyessaldias <jpreyess@uc.cl>
2019-05-18 22:42:33 -0400

    probando imagen facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@
 Devise.setup do |config|
 
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
+  secure_image_url: true,
   info_fields: 'profile_pic'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
